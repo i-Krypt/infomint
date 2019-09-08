59ecbfe7007a5d62e3267d5d8e18ee50c97ae02d
i-Krypt <vamay505@gmail.com>
2019-09-08 23:31:58 +0300

    added parameters to the class news

--- app/models/news.py
+++ app/models/news.py
@@ -1,23 +1,15 @@
-import unittest
-from models import news
-News = news.News
-
-
-class NewsTest(unittest.TestCase):
+class News:
     '''
-    Test Class to test the behaviour of the news class
+    news class to define news objects
     '''
 
-    def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_movie = News(1234, 'certain gibberish', 'A thrilling new Python Series',
-                               'https://image.tmdb.org/t/p/w500/khsjha27hbs', 8.5, 129993)
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_news, News))
-
-
-if __name__ == '__main__':
-    unittest.main()
+   
+    def __init__(self,id,name,author,description,url,urlToImage,publishedAt):
+        self.id =id
+        self.name = name
+        self.author = author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        
